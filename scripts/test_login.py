7410fbd2d00f740fafccf096fc1509227ad59d04
liuzhaowen <liuzhaowen007@163.com>
2019-10-07 18:12:56 +0800

    第一次提交

---
+++ scripts/test_login.py
@@ -0,0 +1,13 @@
+class TestLogin:
+
+    def test_login_01(self):
+        assert 1
+
+    def test_login_02(self):
+        assert 0
+
+    def test_login_03(self):
+        assert 1
+
+    def test_login_04(self):
+        assert 1
